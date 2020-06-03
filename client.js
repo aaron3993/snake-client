7422f921cecc42aee9c32bf566d752d7090552e8
Aaron <aaron3993@hotmail.com>
2020-06-03 18:25:56 +0000

    Added setupInput and handleUserInput files, also added a move up function

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+// const stdin = process.stdin
 
 /**
  * Establishes connection with the game server
@@ -6,14 +7,37 @@ const net = require('net');
 const connect = function() {
   const conn = net.createConnection({ 
     host: '135.23.222.148',
-    port: 50541
+    port: 50541,
   });
   // interpret incoming data as text
+  conn.on('data', () => {
+    console.log("Successfully connected to game server")
+  })
+  conn.on('connect', () => {
+    conn.write('Name: HEY')
+  })
   conn.setEncoding('utf8'); 
+  // stdin.on('data', (key) => {
+  //     console.log(key)
+  conn.write('Move: up')
+  setTimeout(() => {
+    conn.write('Move: up')
+  }, 30)
+  setTimeout(() => {
+    conn.write('Move: up')
+  }, 30)
+  setTimeout(() => {
+    conn.write('Move: up')
+  }, 30)
+  setTimeout(() => {
+    conn.write('Move: up')
+  }, 30)
+
+  // })
   conn.on('data', data => {
     console.log(data)
   })
   return conn;
 }
 
-module.exports = connect
+module.exports = { connect }
